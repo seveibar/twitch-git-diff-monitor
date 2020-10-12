29fecf27d4bd2ea5ae3c258eb38cd444399fe1f9
seveibar <seveibar@gmail.com>
2020-10-12 03:36:12 -0400

    remove old commented code

--- packages/server/app.js
+++ packages/server/app.js
@@ -6,25 +6,9 @@ var parse = require("parse-diff")
 const repoPath = "/home/seve/workspace/os/twitch-git-diff-monitor"
 
 module.exports = async (req, res) => {
-  // console.log("repo state", )
-
   const output = execSync(`git diff`, {
     cwd: repoPath,
   }).toString()
 
   return send(res, 200, parse(output))
-  // send(res, 200, {
-  //   gitDiffs: [
-  //     {
-  //       file: "/package.json",
-  //       diffLines: [
-  //         {
-  //           op: "add",
-  //           line: 30,
-  //           text: `"main": "index.js"`,
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // })
 }
