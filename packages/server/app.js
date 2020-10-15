3f9a8342bfa698038ae912b0dd15fe9e5d3b0b61
seveibar <seveibar@gmail.com>
2020-10-15 05:30:07 -0400

    return newCode / oldCode diffs

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,14 +1,28 @@
 const { send, json } = require("micro")
 const range = require("lodash/range")
 const execSync = require("child_process").execSync
+const fs = require("fs")
 var parse = require("parse-diff")
 
 const repoPath = process.cwd()
 
 module.exports = async (req, res) => {
-  const output = execSync(`git diff`, {
+  const gitDiffOutput = execSync(`git diff`, {
     cwd: repoPath,
   }).toString()
 
-  return send(res, 200, parse(output))
+  const fileDiffs = parse(gitDiffOutput)
+
+  const responseArray = []
+
+  for (const fileDiff of fileDiffs) {
+    responseArray.push({
+      filePath: fileDiff.to,
+      numberOfChanges: fileDiff.additions + fileDiff.deletions,
+      oldCode: execSync(`git show "HEAD:${fileDiff.to}"`).toString(),
+      newCode: fs.readFileSync(fileDiff.to).toString(),
+    })
+  }
+
+  return send(res, 200, responseArray)
 }
