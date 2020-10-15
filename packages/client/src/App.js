10c2ceb878f47f7c3332e278a8eaabf8e52ae13e
seveibar <seveibar@gmail.com>
2020-10-15 04:59:32 -0400

    removed expanding buttons

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,24 +5,10 @@ import "./App.css"
 import "highlightjs/dracula.css"
 import "dracula-prism/css/dracula-prism.css"
 
+import FileDeck from "./components/FileDeck"
+
 const Container = styled("div")({
   overflow: "hidden",
-})
-const Header = styled("div")({
-  backgroundColor: colors.blue[800],
-  color: "#fff",
-  fontSize: 12,
-  padding: 6,
-  fontWeight: 600,
-})
-const Changes = styled("div")({
-  backgroundColor: "#fff",
-  whiteSpace: "pre",
-  fontSize: 12,
-  overflow: "hidden",
-  "& *": {
-    fontFamily: "Monospace",
-  },
 })
 const Change = styled("div")(({ op }) => ({
   color: "#fff",
@@ -40,99 +26,25 @@ const Change = styled("div")(({ op }) => ({
       }),
 }))
 
-const hashChunk = (chunk) => {
-  return `${chunk.filePath},${chunk.lineStart},${chunk.numberLines}`
-}
-
-const findAnyChangedChunks = (lastChunks, newChunks) => {
-  const hashesOfLastChunks = new Set()
-
-  for (const chunk of lastChunks) {
-    hashesOfLastChunks.add(hashChunk(chunk))
-  }
-
-  for (const chunk of newChunks) {
-    if (!hashesOfLastChunks.has(hashChunk(chunk))) {
-      return chunk
-    }
-  }
-
-  return null
-}
-
-const convertToJustChunks = (changedFiles) => {
-  const justChunks = []
-  for (const changedFile of changedFiles) {
-    for (const chunk of changedFile.chunks) {
-      justChunks.push({
-        filePath: changedFile.to,
-        lineStart: chunk.newStart,
-        numberLines: chunk.changes.filter((c) => c.type !== "normal").length,
-        lines: chunk.changes.map((c) => ({ op: c.type, content: c.content })),
-      })
-    }
-  }
-  return justChunks
-}
-
 const App = () => {
-  const [latestChunk, setLatestChunk] = useState(null)
+  const [files, setFiles] = useState(null)
   useEffect(() => {
-    let lastLoadedChunks = []
-    try {
-      lastLoadedChunks = JSON.parse(
-        window.localStorage.getItem("lastLoadedChunks")
-      )
-    } catch (e) {}
-
-    if (!lastLoadedChunks || !lastLoadedChunks.length) {
-      lastLoadedChunks = []
-    }
-
     async function loadDiffs() {
       const res = await fetch("/api").then((r) => r.json())
-
-      const newChunks = convertToJustChunks(res)
-
-      const changedChunk = findAnyChangedChunks(lastLoadedChunks, newChunks)
-      if (changedChunk) {
-        setLatestChunk(changedChunk)
-      }
-
-      if (newChunks.length === 0) {
-        setLatestChunk(null)
-      }
-
-      lastLoadedChunks = newChunks
-      window.localStorage.setItem(
-        "lastLoadedChunks",
-        JSON.stringify(lastLoadedChunks)
-      )
+      setFiles(res)
 
       setTimeout(() => {
         loadDiffs()
-      }, 500)
+      }, 2000)
     }
     loadDiffs()
   }, [])
 
-  if (!latestChunk)
-    return (
-      <Container>
-        <Header>No Recent Changes</Header>
-      </Container>
-    )
+  if (!files) return <Container></Container>
 
   return (
     <Container>
-      <Header>Last Change: {latestChunk.filePath}</Header>
-      <Changes>
-        {latestChunk.lines.map((line, i) => (
-          <Change key={i} op={line.op}>
-            {line.content.slice(1)}
-          </Change>
-        ))}
-      </Changes>
+      <FileDeck files={files} />
     </Container>
   )
 }
--- packages/client/src/components/File/index.js
+++ packages/client/src/components/File/index.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import { styled } from "@material-ui/core"
 import colors from "../../colors"
 import Highlight from "react-highlight.js"
 import DiffViewer from "react-diff-viewer"
 import Prism from "prismjs"
+import Measure from "react-measure"
 
 const Container = styled("div")({
   color: colors.Foreground,
@@ -12,6 +13,7 @@ const Container = styled("div")({
   width: 360,
   height: 360,
   borderRadius: 4,
+  overflow: "hidden",
   padding: 16,
   "& .title": {
     fontWeight: 600,
@@ -23,6 +25,8 @@ const Container = styled("div")({
   },
 })
 
+const Mover = styled("div")({})
+
 const highlightSyntax = (str) => (
   <pre
     style={{ display: "inline" }}
@@ -33,20 +37,41 @@ const highlightSyntax = (str) => (
 )
 
 export const File = ({ filePath, numberOfChanges, oldCode, newCode }) => {
+  const [diffViewerDims, setDiffViewerDims] = useState()
   return (
     <Container>
       <div className="title">
         {filePath}
         <span className="changesText">(±{numberOfChanges})</span>
       </div>
-      <DiffViewer
-        oldValue={oldCode}
-        newValue={newCode}
-        splitView={false}
-        hideLineNumbers={true}
-        useDarkTheme
-        renderContent={highlightSyntax}
-      />
+      <Mover>
+        <Measure
+          bounds
+          onResize={(contentRect) => {
+            // setDiffViewerDims(contentRect.bounds)
+          }}
+        >
+          {({ measureRef }) => (
+            <div ref={measureRef}>
+              <DiffViewer
+                oldValue={oldCode}
+                newValue={newCode}
+                splitView={false}
+                hideLineNumbers={true}
+                disableWordDiff
+                useDarkTheme
+                codeFoldMessageRenderer={() => ""}
+                renderContent={highlightSyntax}
+                styles={{
+                  codeFold: {
+                    height: 0,
+                  },
+                }}
+              />
+            </div>
+          )}
+        </Measure>
+      </Mover>
     </Container>
   )
 }
