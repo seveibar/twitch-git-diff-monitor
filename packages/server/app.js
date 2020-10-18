ceacd8e726d5151733e9b7243bf995877b3e796b
seveibar <seveibar@gmail.com>
2020-10-18 05:57:46 -0400

    fix: skip deleted files, allow repo path to be passed as env var

--- packages/server/app.js
+++ packages/server/app.js
@@ -3,8 +3,9 @@ const range = require("lodash/range")
 const execSync = require("child_process").execSync
 const fs = require("fs")
 var parse = require("parse-diff")
+const path = require("path")
 
-const repoPath = process.cwd()
+const repoPath = process.env.REPO_PATH || process.cwd()
 
 const ignoreFilesEndingWith = ["yarn.lock", "package-lock.json"]
 
@@ -25,11 +26,27 @@ module.exports = async (req, res) => {
     ) {
       continue
     }
+
+    if (fileDiff.to === "/dev/null") {
+      // TODO handle files that are deleted
+      continue
+    }
+
+    let newCode
+    try {
+      newCode = fs.readFileSync(path.join(repoPath, fileDiff.to)).toString()
+    } catch (e) {
+      console.log(`Couldn't read file: "${fileDiff.to}"`)
+      continue
+    }
+
     responseArray.push({
       filePath: fileDiff.to,
       numberOfChanges: fileDiff.additions + fileDiff.deletions,
-      oldCode: execSync(`git show "HEAD:${fileDiff.to}"`).toString(),
-      newCode: fs.readFileSync(fileDiff.to).toString(),
+      oldCode: execSync(`git show "HEAD:${fileDiff.to}"`, {
+        cwd: repoPath,
+      }).toString(),
+      newCode,
     })
   }
 
